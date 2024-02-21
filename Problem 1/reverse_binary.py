6b91831e468f1f747465759657290a5d9ae47d3f
Kai Jameson <jamesonkai0413@gmail.com>
2024-02-20 23:54:08 -0600

    idk

--- Problem 1/reverse_binary.py
+++ Problem 1/reverse_binary.py
@@ -1,8 +1,8 @@
 """
 Complete the following python code to print the reverse binary representation of positive integer number entered by the user.
 
-Name:
-Lab Time:
+Name: Kai Jameson
+Lab Time: Thursdays @ 2pm
 
 """
 
@@ -10,6 +10,12 @@ Lab Time:
 def reverse_binary():
     user_num = int(input())
     # YOUR CODE HERE
+    binary_val = ''
+    while user_num > 0:
+        place = user_num % 2
+        binary_val += str(place)
+        user_num = user_num // 2
+    print(binary_val)
 
 if __name__ == "__main__":
     reverse_binary()
